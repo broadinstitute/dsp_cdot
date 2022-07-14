b70b458a6772a10999dd7fc3cbd2910d8a4d8eb4
Jordan Safer <jordansafer2@gmail.com>
2022-07-14 00:29:42 -0400

    simple script to run fpocket on all PDBs

---
+++ fpocket_all.sh
@@ -0,0 +1,16 @@
+
+# Setup:
+# 1. Install docker and pull down the fpocket/fpocket image
+# 2. Create a data dir with open access (chmod +777 data) and store all PDBs inside
+
+# Make output dir
+mkdir -p fpocket_out
+
+# Loop through all PDB files and run fpocket on each one
+for pdb_file in $(ls data/*.pdb)
+do
+  echo $pdb_file # check filename
+  echo ${pdb_file%.*}_out # check output filename is as expected
+  docker run -v `pwd`:/WORKDIR -w /WORKDIR fpocket/fpocket fpocket -f $pdb_file # run fpocket on the file
+  mv ${pdb_file%.*}_out fpocket_out # move outputs to results dir
+done
